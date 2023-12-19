a2a3a6af84335b9acd3b190b74702ee0057c5423
Cole Gawin <colegawin@gmail.com>
2023-12-19 10:45:12 -0600

    chore: add Rubik 500 weight

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Rubik:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Rubik:ital,wght@0,400;0,500;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
         <link
